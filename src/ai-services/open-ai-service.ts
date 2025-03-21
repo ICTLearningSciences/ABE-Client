3bee51479d6c02d4db445bfcd4e6f10f24f8d3df
Aaron Shiel <ashiel408@gmail.com>
2025-03-20 18:55:46 -0700

    package upgrades

--- src/ai-services/open-ai-service.ts
+++ src/ai-services/open-ai-service.ts
@@ -4,11 +4,11 @@ Permission to use, copy, modify, and distribute this software and its documentat
 
 The full terms of this copyright and license should always be found in the root directory of this software deliverable as "license.txt" and if these terms are not found with this software, please contact the USC Stevens Center for the full license.
 */
+
 import {
   ResponseCreateParamsNonStreaming,
   Response,
-} from 'openai/resources/responses/responses.js';
-
+} from 'openai/resources/responses/responses';
 import {
   AiResponseType,
   AiJobStatusType,
